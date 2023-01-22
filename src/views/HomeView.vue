c0d8e1ee8d6b26aba8829695e63b1ccaa3813fe2
RikuMats <al18108@shibaura-it.ac.jp>
2023-01-22 11:27:49 +0900

    enable home view to feth red books

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -18,26 +18,49 @@ import { RedBook } from "../models/book";
   },
 })
 export default class HomeView extends Vue {
-  private bookList!: Array<RedBook>;
+  private bookList: Array<RedBook> = [];
   public created() {
     //最近読まれた本をサーバーから取得想定
     //サンプルデータ
-    this.bookList = [
-      new RedBook(
-        "9784198942304",
-        "アキラとあきら",
-        "池井戸潤",
-        "http://books.google.com/books/content?id=jo75swEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api",
-        "that's interesting"
-      ),
-      new RedBook(
-        "9784167110116",
-        "手紙",
-        "東野圭吾",
-        "http://books.google.com/books/content?id=_XH4PgAACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api",
-        "that's fun"
-      ),
-    ];
+    fetch("http://localhost:8080/fetchRedBooks.php")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(response.statusText);
+        }
+        return response.json();
+      })
+      .catch((error) => {
+        // ネットワークエラーでも !response.ok でもここで処理できる
+        console.error("エラーが発生しました", error);
+      })
+      .then((apiData) => {
+        for (let redBook of apiData) {
+          let book = new RedBook(
+            redBook["isbn"],
+            redBook["title"],
+            redBook["author"],
+            redBook["img_url"],
+            redBook["impression"]
+          );
+          this.bookList.push(book);
+        }
+      });
+    // this.bookList = [
+    //   new RedBook(
+    //     "9784198942304",
+    //     "アキラとあきら",
+    //     "池井戸潤",
+    //     "http://books.google.com/books/content?id=jo75swEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api",
+    //     "that's interesting"
+    //   ),
+    //   new RedBook(
+    //     "9784167110116",
+    //     "手紙",
+    //     "東野圭吾",
+    //     "http://books.google.com/books/content?id=_XH4PgAACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api",
+    //     "that's fun"
+    //   ),
+    // ];
   }
 }
 </script>
